96bcbadf4ff7e4bdb4e96a23003e8f3726b1f4a7
vitalii-t12 <vitalii.toderian@gmail.com>
2025-12-02 13:21:11 +0200

    fix: app server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
